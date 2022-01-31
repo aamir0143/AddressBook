96f31ad752ee24a00568b895ad49c27d5ed505d4
aamir0143 <mdaamirreza00@gmail.com>
2022-01-31 22:24:00 +0530

    [Aamir9] Add : Added Create a Contact in Address

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("==========Welcome To Addess Book Programs==========");
-Console.ReadLine();
+using System;
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To Addess Book Programs==========");
+            AddressBookEntry addressBook = new AddressBookEntry();
+            Contact newContact = addressBook.CreateContact();
+            Console.WriteLine("*****-> Details Of Person  <-*****");
+            Console.WriteLine($"First Name: {newContact.FirstName}");
+            Console.WriteLine($"Last Name: {newContact.LastName}");
+            Console.WriteLine($"City Name: {newContact.City}");
+            Console.WriteLine($"State Name: {newContact.State}");
+            Console.WriteLine($"Zipcode: {newContact.ZipCode}");
+            Console.WriteLine($"PhoneNumber: {newContact.PhoneNumber}");
+            Console.WriteLine($"Email: {newContact.Email}");
+            Console.WriteLine("==========================================================================");
+            Console.ReadLine();
+        }
+    }
+}
