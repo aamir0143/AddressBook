176c67129cdf8dcb618711922e4bf6e0d98361b4
aamir0143 <mdaamirreza00@gmail.com>
2022-02-19 01:52:52 +0530

    [Aamir] Add : Delete the person

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,22 +1,21 @@
-﻿using AddressBook;
-Console.Write("Select Number:\n1)AddContacts\n2)EditContact\n3)DeleteContact\n");
-int option = Convert.ToInt32(Console.ReadLine());
-switch (option)
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AddressBook
 {
-    case 1:
-        new CreateNewContact().Show();
-        break;
-    case 2:
-        EditEntry.NewContact();
-        EditEntry.ListAllContacts();
-        EditEntry.Update();
-        break;
-    case 3:
-        DeleteContact.NewContact();
-        DeleteContact.ListAllContacts();
-        DeleteContact.Delete();
-        break;
-    default:
-        Console.Write("Please Select Correct Number");
-        break;
+    class Program
+    {
+        //Main  Method / program Entry Point
+        static void Main(string[] args)
+        {
+            //Showing Welcome Message On console.
+            Console.WriteLine("==========Welcome to Address Book Programs==========");
+            AddressBook createAddressBook = new AddressBook();
+            createAddressBook.ReadInput();
+            Console.ReadLine();
+
+        }
+    }
 }
