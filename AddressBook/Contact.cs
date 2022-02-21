54fc7b88a819e111375042a97b39124f7a8efeb7
aamir0143 <mdaamirreza00@gmail.com>
2022-02-21 20:59:39 +0530

    [Aamir] Add : Ability to Create a Contacts in Address Book

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AddressBook
+{
+    public class Contact
+    {
+        //Declaring contact details properties
+        public readonly string firstName;
+        public readonly string lastName;
+        public readonly string address;
+        public readonly string city;
+        public readonly string state;
+        public readonly int zip;
+        public readonly long phoneNumber;
+        public readonly string emailId;
+        public Contact(string firstName, string lastName, string address, string city, string state, int zip, long phoneNumber, string emailId)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.emailId = emailId;
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,31 +1,23 @@
-﻿using System;
+﻿using AddressBook;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace AddressBook
+namespace AddressBookProgram
 {
     public class Program
     {
-        //Main  Method / program Entry Point
-        static void Main(string[] args)
+        //Initializing variable
+        public static int count = 0;
+        public static void Main(string[] args)
         {
-            //Showing Welcome Message On console.
-            Console.WriteLine("==========Welcome To Addess Book Programs==========");
-            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
-            AddressBookEntry addressBook = new AddressBookEntry()  //Initializing elements using collection-initializer syntax
-            {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Convert.ToDouble(Console.ReadLine()),
-                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
-                Email = Console.ReadLine(),
-            };
-            addressBook.displayContact();
-            Console.ReadLine();
+            //Display the welcome message.
+            Console.WriteLine("==========Welcome To Address Book Program==========");
+            //Creating a contact with person details(UC1)
+            AddressBookEntry addressBook = new AddressBookEntry();
+            addressBook.AddContactDetails("Aamir", "Reza", "Dubaili", "Purnea", "Bihar", 854330, 7975633784, "aamirreza156@gmail.com");
+            addressBook.ViewContact();
         }
     }
 }
