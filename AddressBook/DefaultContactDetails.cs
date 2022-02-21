ba9b72fc8ba484931b3bd63ee9f937f9a680d8a9
aamir0143 <mdaamirreza00@gmail.com>
2022-02-21 23:13:58 +0530

    [Aamir] Add : Ability to edit exixting contact person

---
+++ AddressBook/DefaultContactDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AddressBook
+{
+    public class DefaultContactDetails
+    {
+        public static void AddPersonContact(AddressBookEntry addressBook)
+        {
+            addressBook.AddContactDetails("Aamir", "Reza", "Dubaili", "Purnea", "Bihar", 854330, 7975633784, "aamirreza156@gmail.com");
+            addressBook.AddContactDetails("Wasim", "Anwar", "Modhipur", "Rampur", "jharkand", 854331, 7761621183, "aamirreza00@gmail.com");
+            addressBook.AddContactDetails("Shahab", "Alam", "sec_56", "Silampur", "Delhi", 560032, 9199523544, "abc789@gmail.com");
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,16 +5,51 @@ using System.Text;
 using System.Threading.Tasks;
 namespace AddressBook
 {
-    class Program
+    public class Program
     {
-        //Main  Method for program Entry Point
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //Showing Welcome Message On console.
-            Console.WriteLine("==========Welcome To Address Book Programs==========");
-            AddressBook createAddressBook = new AddressBook();
-            createAddressBook.ReadInput();
-            Console.ReadLine();
+            //Display the Welcome Message
+            Console.WriteLine("==========Welcome To Address Book Program==========");
+            //Create objects
+            AddressBookEntry addressBook = new AddressBookEntry(); 
+            while (true)
+            {
+                Console.WriteLine("1: Add A New Person Details" +
+                                  "\n2: Add Default Contact Details" +
+                                  "\n3: View Person Details" +
+                                  "\n4: Edit Person Details" +
+                                  "\n4: Exit"
+                                  );
+                Console.Write("Enter The Choice From Above : ");
+                int userChoice = int.Parse(Console.ReadLine());
+                switch (userChoice)
+                {
+                    case 1:
+                        //Creating a new contact with person details(UC2) 
+                        AddContact.PersonDetails(addressBook);
+                        break;
+                    case 2:
+                        //Creating a contact with person details(UC1) 
+                        DefaultContactDetails.AddPersonContact(addressBook);
+                        break;
+                    case 3:
+                        addressBook.ViewContact();
+                        break;
+                    case 4:
+                        //Editing a contact details with given name(UC3)
+                        Console.Write("Enter The First Name Exactly To Edit : ");
+                        string fName = Console.ReadLine();
+                        addressBook.EditContact(fName);
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Enter A Right Choice");
+                        continue;
+                }
+            }
         }
     }
 }
