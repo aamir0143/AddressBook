6244e291e544c95ba91f2e16d94553dec044b211
aamir0143 <mdaamirreza00@gmail.com>
2022-02-22 23:29:05 +0530

    [Aamir] Add : Maintain Dict for Persons city and state

--- AddressBook/AddressBookEntry.cs
+++ AddressBook/AddressBookEntry.cs
@@ -13,8 +13,9 @@ namespace AddressBook
         //Creating a contact list
         private readonly List<Contact> contactList;
         private readonly Dictionary<string, AddressBookEntry> addressContactBook;
-
-        public AddressBookEntry() 
+        private Dictionary<Contact, string> personsCity = new Dictionary<Contact, string>();
+        private Dictionary<Contact, string> personsState = new Dictionary<Contact, string>();
+        public AddressBookEntry()
         {
             contactList = new List<Contact>();
             addressContactBook = new Dictionary<string, AddressBookEntry>();
@@ -167,12 +168,23 @@ namespace AddressBook
             }
             return book;
         }
+        //Method to add a new list of values from multiple dictionary's(UC9)
+        public List<Contact> GetListOfMulAddressBookValues(Dictionary<Contact, string> dictionaryName)
+        {
+            List<Contact> book = new List<Contact>();
+            foreach (var value in dictionaryName.Keys)
+            {
+                book.Add(value);
+            }
+            return book;
+        }
         //Method to search the person by city(UC8)
         public void SearchPersonByCity(string city)
         {
+            CreateCityDictionary();
             foreach (AddressBookEntry addrBookObj in addressContactBook.Values)
             {
-                List<Contact> contactList = GetListOfMulAddressBookValues(addrBookObj.contactList);
+                List<Contact> contactList = GetListOfMulAddressBookValues(addrBookObj.personsCity);
                 foreach (Contact contact in contactList.FindAll(c => c.city.Equals(city)).ToList())
                 {
                     Console.WriteLine(contact.ToString());
@@ -182,12 +194,35 @@ namespace AddressBook
         //Method to search the person by state(UC8)
         public void SearchPersonByState(string state)
         {
-            foreach (AddressBookEntry addressbookobj in addressContactBook.Values)
+            CreateStateDictionary();
+            foreach (AddressBookEntry addressBookobj in addressContactBook.Values)
             {
-                List<Contact> contactList = GetListOfMulAddressBookValues(addressbookobj.contactList);
+                List<Contact> contactList = GetListOfMulAddressBookValues(addressBookobj.personsState);
                 foreach (Contact contact in contactList.FindAll(c => c.state.Equals(state)).ToList())
                 {
                     Console.WriteLine(contact.ToString());
+                }
+            }
+        }
+        //Method to maintain dictionary of city and person(UC9)
+        public void CreateCityDictionary()
+        {
+            foreach (AddressBookEntry addressBookObj in addressContactBook.Values)
+            {
+                foreach (Contact contact in addressBookObj.contactList)
+                {
+                    addressBookObj.personsCity.Add(contact, contact.city);
+                }
+            }
+        }
+        //Method to maintain dictionary of state and person(UC9)
+        public void CreateStateDictionary()
+        {
+            foreach (AddressBookEntry addressBookObj in addressContactBook.Values)
+            {
+                foreach (Contact contact in addressBookObj.contactList)
+                {
+                    addressBookObj.personsState.Add(contact, contact.state);
                 }
             }
         }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -11,7 +11,7 @@ namespace AddressBook
         public static void Main(string[] args)
         {
             //Display Welcome Message
-            Console.WriteLine("==========Welcome To Address Book Programs=========");
+            Console.WriteLine("Welcome To Address Book Program");
             //Create Objects
             AddressBookEntry addressBook = new AddressBookEntry();
             while (true)
